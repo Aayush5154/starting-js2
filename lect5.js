bf1c65836da58c93b2ea322a68c7664afdd94b63
Aayush5154 <146195691+Aayush5154@users.noreply.github.com>
2025-03-09 13:25:03 +0000

    conversion of data types

---
+++ lect5.js
@@ -0,0 +1,12 @@
+let score = 123234
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let value = String(score)
+console.log(typeof(value))
+console.log(score)
+
+//1 = true 0 = false 
+//""(empty string) = false 
+//"hitesh" = true  
