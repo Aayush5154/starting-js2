aa0a2c408b6fee63d524f406cd4b5e222ab8b25b
Aayush5154 <146195691+Aayush5154@users.noreply.github.com>
2025-03-10 12:12:01 +0000

    conversion

--- lect8.js
+++ lect8.js
@@ -1,14 +1,33 @@
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
 
-console.log(undefined == 0)
-console.log(undefined >= 0)
-console.log(undefined > 0)
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+// console.log(undefined > 0)
 
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
-console.log(id === anotherId)
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId)
 
-console.log(id);//this 
+// console.log(id);//this 
+
+
+let myObj = function(){
+    console.log("hello world")
+}
+
+myObj();
+
+const bigNumber = 246384538464;
+
+console.log(typeof(bigNumber))
+
+const avj = null
+let meet;
+console.log(typeof(avj))//here if null is assgned to any variable 
+console.log(typeof(meet))
+
+
+
