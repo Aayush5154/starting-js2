c8f035f270eff850f981552a6c4986940d4d7381
Aayush5154 <146195691+Aayush5154@users.noreply.github.com>
2025-03-10 12:01:27 +0000

    comparasion

---
+++ lect8.js
@@ -0,0 +1,14 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0)
+console.log(undefined >= 0)
+console.log(undefined > 0)
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId)
+
+console.log(id);//this 
